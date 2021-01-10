07d4b84e915807b8d23e711330eb3698aa64bca0
Patrick Batenburg <p.batenburg@live.nl>
2021-01-10 10:50:43 +0100

    Adding json path to coverall

--- coverage-checker.php
+++ coverage-checker.php
@@ -16,15 +16,15 @@ $totalElements   = 0;
 $checkedElements = 0;
 
 foreach ($metrics as $metric) {
-$totalElements   += (int) $metric['elements'];
-$checkedElements += (int) $metric['coveredelements'];
+    $totalElements   += (int) $metric['elements'];
+    $checkedElements += (int) $metric['coveredelements'];
 }
 
 $coverage = ($checkedElements / $totalElements) * 100;
 
 if ($coverage < $percentage) {
-echo 'Code coverage is ' . $coverage . '%, which is below the accepted ' . $percentage . '%' . PHP_EOL;
-exit(1);
+    echo 'Code coverage is ' . $coverage . '%, which is below the accepted ' . $percentage . '%' . PHP_EOL;
+    exit(1);
 }
 
 echo 'Code coverage is ' . $coverage . '% - OK!' . PHP_EOL;
